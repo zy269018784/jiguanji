03d9d4dd26e3b90d7d7808242a062050078c4f78
zhouyue <269018784@qq.com>
2025-12-05 07:20:51 +0800

    main

--- Source/main.cpp
+++ Source/main.cpp
@@ -1,12 +1,28 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
-
+#include <QQuickWidget>
+#include  <QDebug>
 int main(int argc, char* argv[])
 {
     QGuiApplication app(argc, argv);
 
+#if 1
+    qDebug() << "hello1";
+    QQuickWidget *view = new QQuickWidget;    return 0;
+    if (!view->engine()) {
+        qDebug() << "QML引擎初始化失败";
+        return -1;
+    }
+    qDebug() << "hello2";
+    view->resize(800, 600);
+    view->setSource(QUrl::fromLocalFile("MyQML/MyWindow.qml"));
+    view->show();
+    qDebug() << "hello3";
+#else
     QQmlApplicationEngine engine;
     engine.load(QUrl(QStringLiteral("MyQML/MyWindow.qml")));
+#endif
+
     int ret = app.exec();
     return ret;
 }
